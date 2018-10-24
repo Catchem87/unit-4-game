5c4f3663ed876cc24912126be0626d39beadb49b
Catchem87 <moras.ashley87@gmail.com>
2018-10-24 18:34:32 -0400

    number generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,21 @@ $(document).ready(function() {
     $("#randNum").html(randomNum)
     // Generate random number between 1 and 12
         // store and assign to blue crystal
+    var blueNum = Math.floor(Math.random() * 12) + 1
+    console.log(blueNum)
+    $("")
     // Generate random number between 1 and 12
         // store and assign to pink crystal
+    var pinkNum = Math.floor(Math.random() * 12) + 1
+    console.log(pinkNum)
     // Generate random number between 1 and 12
         // store and assign to green crystal
+    var greenNum = Math.floor(Math.random() * 12) + 1
+    console.log(greenNum)
     // Generate random number between 1 and 12
         // store and assign to yellow crystal
+    var yellowNum = Math.floor(Math.random() * 12) + 1
+    console.log(yellowNum)
         
     // User clicks on a crystal
         // add value to player's total score
