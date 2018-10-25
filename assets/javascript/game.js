4e0cc39c1fbabc13d476829174a7397980e01c8a
Catchem87 <moras.ashley87@gmail.com>
2018-10-25 15:22:46 -0400

    udpates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,77 @@
-$(document).ready(function() {
-    // Your code goes here
-
-    // On window load -->
-
-    // Generate random number between 19 and 120
-        // store and display
+$(document).ready(function () {
+   
     var randomNum = Math.floor(Math.random() * 120) + 19
     console.log(randomNum)
     $("#randNum").html(randomNum)
-    // Generate random number between 1 and 12
-        // store and assign to blue crystal
+   
     var blueNum = Math.floor(Math.random() * 12) + 1
     console.log(blueNum)
-    $("")
-    // Generate random number between 1 and 12
-        // store and assign to pink crystal
     var pinkNum = Math.floor(Math.random() * 12) + 1
     console.log(pinkNum)
-    // Generate random number between 1 and 12
-        // store and assign to green crystal
     var greenNum = Math.floor(Math.random() * 12) + 1
     console.log(greenNum)
-    // Generate random number between 1 and 12
-        // store and assign to yellow crystal
     var yellowNum = Math.floor(Math.random() * 12) + 1
     console.log(yellowNum)
-        
-    // User clicks on a crystal
-        // add value to player's total score
-    
-    // Player wins
-        // player's total score === random number
-        // wins increase by 1
-        // game resets (not wins/losses)
-    // Player loses
-        // player's total score exceeds random number
-        // losses increase by 1
-        // game resets (not wins/losses)
-    });
+
+    var playerScore = 0;
+    var losses = 0;
+    var wins = 0;
+
+    $("#score").html(playerScore)
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+
+
+    $(".blueCrystal").on("click", function () {
+        playerScore += parseInt(blueNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            console.log("WINNER")
+            $("#wins").html(++wins)
+        }
+        if (playerScore > randomNum) {
+            console.log("LOSER")
+            $("#losses").html(++losses)
+        }
+    })
+
+    $(".pinkCrystal").on("click", function () {
+        playerScore += parseInt(pinkNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            alert("Game Won")
+            $("#wins").html(++wins)
+        }
+        if (playerScore > randomNum) {
+            alert("Game Lost")
+            $("#losses").html(++losses)
+        }
+    })
+
+    $(".greenCrystal").on("click", function () {
+        playerScore += parseInt(greenNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            alert("Game Won")
+            $("#wins").html(++wins)
+        }
+        if (playerScore > randomNum) {
+            alert("Game Lost")
+            $("#losses").html(++losses)
+        }
+    })
+
+    $(".yellowCrystal").on("click", function () {
+        playerScore += parseInt(yellowNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            alert("Game Won")
+            $("#wins").html(++wins)
+        }
+        if (playerScore > randomNum) {
+            alert("Game Lost")
+            $("#losses").html(++losses)
+        }
+    })
+
+})
